954fbf5642e4bfc6407bd58e0102740ea7bcecfe
JAFAR1234567 <abmamun437@gmail.com>
2023-08-04 22:01:20 +0600

    assignment task

---
+++ assignment/fourth_project/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    return render(request,"base.html")
+
+def about(request):
+    return render(request,"about.html")
